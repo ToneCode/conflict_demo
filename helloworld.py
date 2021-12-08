d88f4882eab47ecf250164decd5f7c7424b7213c
Anthony Williams <mylifeassurance@aol.com>
2021-12-07 17:48:36 -0800

    add city state

--- helloworld.py
+++ helloworld.py
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-<< << << < HEAD
-
-
 def hello_world(city, state):
-    print(f'Hello World {state}!')
-
-
-== == == =
-
-
-def hello_world():
-    print(f'Hello World {city}, {state}!')
-
-
->>>>>> > 23a734ee30acda8b7b6097c8d964609a689d29c7
+    print(f'Hello World {city},{state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 40e4b8ebba334e458191314d27f7bf3e2ce1c6a1
 
 
 main()
